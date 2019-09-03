d3005de4a609438ebbb5a6c3f02303b8f28da742
Nick Hobart <nhobart@signal.co>
2019-09-03 14:12:23 -0500

    removing sha?

--- tmrwppk.rb
+++ tmrwppk.rb
@@ -3,7 +3,6 @@ class Tmrwppk < Formula
   homepage "https://github.com/hobakill/homebrew-tmrwppk"
   url "https://github.com/hobakill/homebrew-tmrwppk/blob/master/dist/tmrwppk.pex", :using => :nounzip
   version "0.0.6"
-  sha256 "f57d6adad6e39c8227622c2a3c65aeac374608f8"
 
   bottle :unneeded
 
