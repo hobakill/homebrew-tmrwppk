e07ce207cf94752d64b1944445b7ad4bb6f482ba
Nick Hobart <nhobart@signal.co>
2019-09-03 13:48:00 -0500

    fixing url

--- tmrwppk.rb
+++ tmrwppk.rb
@@ -1,7 +1,7 @@
 class Tmrwppk < Formula
   desc "Get tomorrow's electricity rates today!"
   homepage "https://github.com/hobakill/homebrew-tmrwppk"
-  url "https://github.com/hobakill/homebrew-tmrwppk/tree/master/dist/tmrwppk.zip", :using => :nounzip
+  url "https://github.com/hobakill/homebrew-tmrwppk/blob/master/dist/tmrwppk.pex", :using => :nounzip
   version "0.0.6"
   sha256 "a2e494395da67d936c3dde81e92328da0490af38676bd4cf0cb64a65880bbb02"
 
