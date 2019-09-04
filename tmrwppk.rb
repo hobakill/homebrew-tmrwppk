bdc9de0e86b2289bb180c17e2ed7401178b276cb
Nick Hobart <nhobart@signal.co>
2019-09-03 19:22:00 -0500

    will this work?

--- tmrwppk.rb
+++ tmrwppk.rb
@@ -1,15 +1,65 @@
 class Tmrwppk < Formula
-  desc "Get tomorrow's electricity rates today!"
-  homepage "https://github.com/hobakill/homebrew-tmrwppk"
-  url "https://github.com/hobakill/homebrew-tmrwppk/raw/master/dist/tmrwppk.pex", :using => :nounzip
-  version "0.0.6"
+  inclue Language::Python::Virtualenv
 
-  bottle :unneeded
+  version "v0.0.6"
+  homepage "https://github.com/hobakill/tmrwppk"
+  url "https://github.com/hobakill/tmrwppk/archive/#{version}.tar.gz"
+  sha256 "fbfa974e1a0f7f0d69ce56d7bb8c3d66d501c05aab816064c9b668e37a6dad47"
+  desc "Tomrrow's Prices today...kinda"
 
-  def install
-    executable = "tmrwppk.pex".split(".pex")[0]
-    system("mv * #{executable} && chmod +x #{executable}")
-    bin.install "#{executable}"
+  depends_on "python"
+
+  resource "arrow" do
+    url "https://files.pythonhosted.org/packages/6f/fd/8d6c15fd0ad1d1f7f41a3bf524b9028bc2521cb619695a445f5e777b1fdb/arrow-0.14.6.tar.gz"
+    sha256 "9f1503d359011a74cd41169652f0eb0232822f58c67c414229f4174a15ae71f9"
   end
 
+  resource "beautifulsoup4" do
+    url "https://files.pythonhosted.org/packages/23/7b/37a477bc668068c23cb83e84191ee03709f1fa24d957b7d95083f10dda14/beautifulsoup4-4.8.0.tar.gz"
+    sha256 "25288c9e176f354bf277c0a10aa96c782a6a18a17122dba2e8cec4a97e03343b"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/c5/67/5d0548226bcc34468e23a0333978f0e23d28d0b3f0c71a151aef9c3f7680/certifi-2019.6.16.tar.gz"
+    sha256 "945e3ba63a0b9f577b1395204e13c3a231f9bc0223888be653286534e5873695"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/ad/13/eb56951b6f7950cadb579ca166e448ba77f9d24efc03edd7e55fa57d04b7/idna-2.8.tar.gz"
+    sha256 "c357b3f628cf53ae2c4c05627ecc484553142ca23264e593d327bcde5e9c3407"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/ad/99/5b2e99737edeb28c71bcbec5b5dda19d0d9ef3ca3e92e3e925e7c0bb364c/python-dateutil-2.8.0.tar.gz"
+    sha256 "c89805f6f4d64db21ed966fda138f8a5ed7a4fdbc1a8ee329ce1b74e3c74da9e"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/01/62/ddcf76d1d19885e8579acb1b1df26a852b03472c0e46d2b959a714c90608/requests-2.22.0.tar.gz"
+    sha256 "11e007a8a2aa0323f5a921e9e6a2d7e4e67d9877e85773fba9ba6419025cbeb4"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
+    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+  end
+
+  resource "soupsieve" do
+    url "https://files.pythonhosted.org/packages/6b/77/b7801323fd321021d92efb11154b7b85410318b8a2e9757c410afb6d976f/soupsieve-1.9.3.tar.gz"
+    sha256 "8662843366b8d8779dec4e2f921bebec9afd856a5ff2e82cd419acc5054a1a92"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/4c/13/2386233f7ee40aa8444b47f7463338f3cbdf00c316627558784e3f542f07/urllib3-1.25.3.tar.gz"
+    sha256 "dbe59173209418ae49d485b87d1681aefa36252ee85884c31346debd19463232"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
 end
